c32e505dfe42ad913c7f4858ea4089c4d18576fd
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-03-25 20:50:52 +0800

    Delete unused code

--- src/DE10_LITE_BUTTON_COUNTER.v
+++ src/DE10_LITE_BUTTON_COUNTER.v
@@ -10,19 +10,6 @@ module DE10_LITE_BUTTON_COUNTER(
 	input 		          		MAX10_CLK1_50,
 	input 		          		MAX10_CLK2_50,
 
-	//////////// SDRAM //////////
-	output		    [12:0]		DRAM_ADDR,
-	output		     [1:0]		DRAM_BA,
-	output		          		DRAM_CAS_N,
-	output		          		DRAM_CKE,
-	output		          		DRAM_CLK,
-	output		          		DRAM_CS_N,
-	inout 		    [15:0]		DRAM_DQ,
-	output		          		DRAM_LDQM,
-	output		          		DRAM_RAS_N,
-	output		          		DRAM_UDQM,
-	output		          		DRAM_WE_N,
-
 	//////////// SEG7 //////////
 	output		     [7:0]		HEX0,
 	output		     [7:0]		HEX1,
@@ -34,36 +21,10 @@ module DE10_LITE_BUTTON_COUNTER(
 	//////////// KEY //////////
 	input 		     [1:0]		KEY,
 
-	//////////// LED //////////
-	output		     [9:0]		LEDR,
-
 	//////////// SW //////////
-	input 		     [9:0]		SW,
-
-	//////////// VGA //////////
-	output		     [3:0]		VGA_B,
-	output		     [3:0]		VGA_G,
-	output		          		VGA_HS,
-	output		     [3:0]		VGA_R,
-	output		          		VGA_VS,
-
-	//////////// Accelerometer //////////
-	output		          		GSENSOR_CS_N,
-	input 		     [2:1]		GSENSOR_INT,
-	output		          		GSENSOR_SCLK,
-	inout 		          		GSENSOR_SDI,
-	inout 		          		GSENSOR_SDO,
-
-	//////////// Arduino //////////
-	inout 		    [15:0]		ARDUINO_IO,
-	inout 		          		ARDUINO_RESET_N,
-
-	//////////// GPIO, GPIO connect to GPIO Default //////////
-	inout 		    [35:0]		GPIO
+	input 		     [9:0]		SW
 
 );
-
-
 
 //=======================================================
 //  REG/WIRE declarations
@@ -72,8 +33,6 @@ module DE10_LITE_BUTTON_COUNTER(
 wire [6:0] count_SW, count_KEY;
 wire [3:0] count_KEY_digit, count_KEY_ten;
 wire [3:0] count_SW_digit, count_SW_ten;
-
-//reg [3:0] digit0, digit1, digit2, digit3, digit4, digit5;
 
 //=======================================================
 //  Structural coding
@@ -84,22 +43,22 @@ assign count_KEY_ten   = (count_KEY[6:0]/10)%10;
 assign count_SW_digit  = count_SW[6:0]%10;
 assign count_SW_ten    = (count_SW[6:0]/10)%10;
 
-//COUNTER_BUTTON ucounter(
-//	.rst(SW[9]),
-//	.KEY(KEY),
-//	.SW(SW[1:0]),
-//	.count_KEY(count_KEY),
-//	.count_SW(count_SW)
-//);
-
-COUNTER_DEBOUNCE ucounter(
-	.clk(MAX10_CLK1_50),
+COUNTER_BUTTON ucounter(
 	.rst(SW[9]),
 	.KEY(KEY),
 	.SW(SW[1:0]),
 	.count_KEY(count_KEY),
 	.count_SW(count_SW)
-);	
+);
+
+// COUNTER_DEBOUNCE ucounter(
+// 	.clk(MAX10_CLK1_50),
+// 	.rst(SW[9]),
+// 	.KEY(KEY),
+// 	.SW(SW[1:0]),
+// 	.count_KEY(count_KEY),
+// 	.count_SW(count_SW)
+// );
 
 SEG7_LUT_6 u_seg(
 	.oSEG0(HEX0),
@@ -108,7 +67,6 @@ SEG7_LUT_6 u_seg(
 	.oSEG3(HEX3),
 	.oSEG4(HEX4),
 	.oSEG5(HEX5),
-	//.iDIG ({ digit5, digit4, digit3, digit2, digit1, digit0})
 	.iDIG({4'he, count_SW_ten, count_SW_digit, 4'he, count_KEY_ten, count_KEY_digit})
 );
 	
--- src/counter_tb_v5.v
+++ src/counter_tb_v5.v
@@ -7,20 +7,6 @@ wire [5:0] count_50;
 wire [3:0] count_10;
 wire       clk_div50, clk_div10;
 
-// counter_v5_50 counter_50(
-//     .clk(clk),
-//     .rst(rst),
-//     .count_50(count_50),
-//     .clk_div50(clk_div50)
-// );
-
-// counter_v5_10 counter_10(
-//     .clk(clk_div50),
-//     .rst(rst),
-//     .count_10(count_10),
-//     .clk_div10(clk_div10)
-// );
-
 counter_v5 counter(
     .clk(clk),
     .rst(rst),
